b9e9bb9c8e85d7459280b290b65f33425c1c3b99
Blake <telefragent@gmail.com>
2020-09-07 13:45:39 -0400

    update

--- Assets/Scripts/LaunchCube.cs
+++ Assets/Scripts/LaunchCube.cs
@@ -4,11 +4,5 @@ using UnityEngine;
 
 public class LaunchCube : MonoBehaviour
 {
-	public Rigidbody RB;
-	public float Power;
-
-	private void OnMouseDown()
-	{
-		RB.AddForce(new Vector3(0f, Power, 0f));
-	}
+	
 }
